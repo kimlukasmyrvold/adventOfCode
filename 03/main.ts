d24c312bb4625a4d04aae2d44179cd3db4785473
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-12-03 20:50:11 +0100

    day 3 part 2

--- 03/main.ts
+++ 03/main.ts
@@ -1,16 +1,30 @@
 import * as fs from 'fs';
 
-const inputPath: { main: string, sample: string } = {
+const inputPath: { main: string, sample: string, sample2: string } = {
     main: './03/input.txt',
     sample: './03/input-sample.txt',
+    sample2: './03/input-sample-2.txt',
 };
 
-const lines = fs.readFileSync(inputPath.main, 'utf-8').trim();
+const matches: string[] = fs.readFileSync(inputPath.main, 'utf-8').trim().match(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/g) || [];
+const result = (): [number, number] => {
+    let enab: boolean = true;
+    let p1: number = 0;
+    let p2: number = 0;
 
-const regex = /mul\(\d+,\d+\)/g;
-const matches = lines.match(regex);
-const result = matches?.map(match => match.split('mul(')[1].slice(0, -1).split(',')
-    .reduce((acc, num) => acc * Number(num), 1))
-    .reduce((acc, num) => acc + num);
+    for (const match of matches) {
+        if (match === "do()") enab = true;
+        else if (match === "don't()") enab = false;
+        else if (match.startsWith('mul(') && match.endsWith(')')) {
+            const [n1, n2] = match.slice(4, -1).split(',').map(Number);
+            const p = n1 * n2;
+            p1 += p;
+            if (enab) p2 += p;
+        }
+    }
 
-console.log('part1:', result);
+    return [p1, p2];
+};
+
+const [p1, p2]: [number, number] = result();
+console.log(`Part1: ${p1}\nPart2: ${p2}`);
